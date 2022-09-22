9031aa995e6847f420cf4c402cda51467ca78e1f
gohooli <gohooli@tencent.com>
2022-09-22 11:05:03 +0800

    feat: 增加备案号

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view />
+    <a href="https://beian.miit.gov.cn/" style="display: block; width: 100%; text-align: center;" target="_blank">粤ICP备2021160166号</a>
   </div>
 </template>
 
