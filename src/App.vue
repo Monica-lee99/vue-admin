b577057bf2486d3b2e78ae15c73eaef357da2510
gohooli <gohooli@tencent.com>
2023-03-21 15:03:07 +0800

    perf: 优化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-view />
-    <a href="https://beian.miit.gov.cn/" style="display: block; width: 100%; text-align: center;" target="_blank">粤ICP备2021160166号</a>
+    <a href="https://beian.miit.gov.cn/" style="display: block; width: 100%; text-align: center; position: absolute; z-index: -1;" target="_blank">粤ICP备2021160166号</a>
   </div>
 </template>
 
